bc14e4eeb7dfef9f08d3e46dc06bd7ad5e663f1e
Gleb Shalimov <sbgleb10@gmail.com>
2021-07-03 17:24:44 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -21,3 +21,28 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 // Smooth Scrolling Sticky ScrollSpy Navigation
+
+
+// menu bar
+ 
+document.addEventListener('DOMContentLoaded', () => {
+	const revealerNav = window.revealer({
+	  revealElementSelector: '.nav-js',
+	  options: {
+		anchorSelector: '.nav-btn-js',
+	  },
+	});
+  
+	const actionBtn = document.querySelector('.nav-btn-js');
+	actionBtn.addEventListener('click', () => {
+	  if (!revealerNav.isRevealed()) {
+		revealerNav.reveal();
+		actionBtn.setAttribute('data-open', true);
+	  } else {
+		revealerNav.hide();
+		actionBtn.setAttribute('data-open', false);
+	  }
+	});
+  });
+
+  // menu bar 
